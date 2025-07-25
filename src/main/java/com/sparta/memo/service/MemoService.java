75a22fe21e8487813cd5700f50252bcc8ef375e2
dayaelee <dayae.dev@gmail.com>
2025-07-25 15:30:56 +0900

    feat: SpringBoot의 JPA

--- src/main/java/com/sparta/memo/service/MemoService.java
+++ src/main/java/com/sparta/memo/service/MemoService.java
@@ -4,7 +4,7 @@ import com.sparta.memo.dto.MemoRequestDto;
 import com.sparta.memo.dto.MemoResponseDto;
 import com.sparta.memo.entity.Memo;
 import com.sparta.memo.repository.MemoRepository;
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,7 +13,7 @@ import java.util.List;
 public class MemoService {
 
     private final MemoRepository memoRepository;
-
+    @Autowired
     public MemoService(MemoRepository memoRepository) {
         this.memoRepository = memoRepository;
     }
